96946ebbd4ee37cb866d4c434c6ddf738b55c35a
tomaszlach <tomaszlach86@gmail.com>
2019-06-07 17:29:47 +0200

    improvements

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,38 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         SubjectArray sa = new SubjectArray(3);
-        sa.fillTab();
+        Scanner scanner = new Scanner(System.in);
+        boolean flag = true;
+        int counter = 0;
+
+        while (flag) {
+
+            System.out.println("Podaj nazwe przedmiotu");
+            String name = scanner.nextLine();
+            System.out.println("Podaj ilość punktów ects");
+            int ects = scanner.nextInt();
+            System.out.println("Podaj ilość godzin w semestrze");
+            int hours = scanner.nextInt();
+
+            Subject sub = new Subject(name, ects, hours);
+            if (!sa.contains(sub)) {
+                sa.add(sub, counter);
+                counter++;
+                System.out.println("obiet został dodany: " + sub);
+            } else {
+                System.out.println("Obiekt nie został dodany! Wpisz dane ponownie");
+            }
+            if (counter >= sa.getTabLength()) {
+                flag = false;
+                System.out.println("tablica jest pełna! Kończymy pracę!");
+            }
+        }
         System.out.println(sa);
 
     }
+
 
 }
--- src/main/java/SubjectArray.java
+++ src/main/java/SubjectArray.java
@@ -1,5 +1,4 @@
 import java.util.Arrays;
-import java.util.Scanner;
 
 public class SubjectArray {
 
@@ -9,32 +8,21 @@ public class SubjectArray {
         this.tab = new Subject[length];
     }
 
-    private boolean contains(Subject subject) {
+    public boolean contains(Subject subject) {
         boolean flag = false;
         for (Subject sub : tab) {
             if (subject.equals(sub)) {
                 flag = true;
+                break;
             }
         }
 
         return flag;
     }
 
-    private void add(Subject subject, int position) {
+    public boolean add(Subject subject, int position) {
         this.tab[position] = subject;
-    }
-
-    private Subject createSubject() {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Podaj nazwe przedmiotu");
-        String name = scanner.nextLine();
-        System.out.println("Podaj ilość punktów ects");
-        int ects = scanner.nextInt();
-        System.out.println("Podaj ilość godzin w semestrze");
-        int hours = scanner.nextInt();
-
-        return new Subject(name, ects, hours);
+        return true;
     }
 
     @Override
@@ -44,26 +32,9 @@ public class SubjectArray {
                 '}';
     }
 
-    private int getTabLength() {
+    public int getTabLength() {
         return this.tab.length;
     }
 
-    void fillTab() {
-        boolean flag = true;
-        int counter = 0;
-        while (flag) {
-            Subject sub = createSubject();
-            if (!contains(sub)) {
-                add(sub, counter);
-                counter++;
-                System.out.println("obiet został dodany: " + sub);
-            } else {
-                System.out.println("Obiekt nie został dodany! Wpisz dane ponownie");
-            }
-            if (counter >= getTabLength()) {
-                flag = false;
-                System.out.println("tablica jest pełna! Kończymy pracę!");
-            }
-        }
-    }
+
 }
